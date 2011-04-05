45fec6c965901f91baa16c513deeb31ed8f7ef49
Alec Story <avs38@cornell.edu>
2011-04-04 22:25:36 -0400

    Basic classifier
    nltk is really clever, and has really easy-to-use classifiers built in for us.
    Naive Bayes and Decision Tree are both included and work splendidly.

---
+++ classify.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import nltk
+train = [
+    (dict(a=1,b=1,c=1), 'y'),
+    (dict(a=1,b=1,c=1), 'x'),
+    (dict(a=1,b=1,c=0), 'y'),
+    (dict(a=0,b=1,c=1), 'x'),
+    (dict(a=0,b=1,c=1), 'y'),
+    (dict(a=0,b=0,c=1), 'y'),
+    (dict(a=0,b=1,c=0), 'x'),
+    (dict(a=0,b=0,c=0), 'x'),
+    (dict(a=0,b=1,c=1), 'y'),
+    ]
+
+test = [
+    (dict(a=1,b=0,c=1)), # unseen
+    (dict(a=1,b=0,c=0)), # unseen
+    (dict(a=0,b=1,c=1)), # seen 3 times, labels=y,y,x
+    (dict(a=0,b=1,c=0)), # seen 1 time, label=x
+    ]
+
+classifier = nltk.NaiveBayesClassifier.train(train)
+print classifier.batch_classify(test)
+print classifier.show_most_informative_features()
