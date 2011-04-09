1079850e9f7a8b2025e9febe14b24628291f5f95
Thomas Levine <thomas.levine@gmail.com>
2011-04-09 18:18:09 -0400

    sample system combinations

--- experiment/combine.R
+++ experiment/combine.R
@@ -1,7 +1,7 @@
 #This takes a list of one or two elements.
 # $files vector of file names
 # $scores data.frame of data (optional)
-score.bind=function(s) {if (length(s$files)==0) s else{
+score.bind=function(s) {if (length(s$files)==0) s$scores else{
 	scores.new=read.csv(s$files[1],header=F,col.names=c('word','instance','correct'))
 	scores.new$id=s$files[1]
 	Recall(list(files=s$files[-1],
@@ -13,3 +13,7 @@ combine=function(...){
 	score.bind(list(files=c(...)))
 }
 
+main=function(){
+	combine(paste('random_data/',c('randomscores01.csv', 'randomscores02.csv', 'randomscores03.csv', 'randomscores04.csv',
+	'randomscores05.csv', 'randomscores06.csv', 'randomscores07.csv', 'randomscores08.csv'),sep=''))
+}
