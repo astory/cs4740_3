a11f7fd9534748a2fee2cebf8181144ecaaf362d
Dustin Tiedemann <djt74@cornell.edu>
2011-04-09 12:07:50 -0400

    made the format match

--- stem_feature.py
+++ stem_feature.py
@@ -4,9 +4,6 @@ from nltk.stem import *
 from nltk.stem.porter import *
 stemmer = PorterStemmer()
 a = []
-def stemCorpus(inputList):
-  for x in inputList:
-    wordList = x.split("\t")
-    word = wordList[0]
-    a.append((x + "\t" + stemmer.stem(word)))
-  return a
+def stemCorpus(context,position,dictionary):
+  dictionary["stem"] = stemmer.stem(context[position])
+  return dictionary
