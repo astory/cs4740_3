c52975ecf989bc2cfa2f4e94e1aceb49579e1ee7
Thomas Levine <thomas.levine@gmail.com>
2011-04-09 17:32:26 -0400

    combining score output files yay

--- experiment/combine.R
+++ experiment/combine.R
@@ -1,32 +1,15 @@
-combine=function(files,features){
-#output must be a vector with values in the format paste(scores,id,sep=''), where id looks like '03.csv'
-	scores.tab=read.csv(files)
-	systems.tab=read.csv(systems)
-}
-
 #This takes a list of one or two elements.
 # $files vector of file names
 # $scores data.frame of data (optional)
-score.bind=function(s) {
-if (length(s$files)==1){
-	#Read the file
-	scores.tab=read.csv(s$files[1])
-	#Use the file name as the identifier
-	scores.tab$id=s$files[1]
-	#Return the list of files minus the one we added
-	#and the data.frame of scores
-	list(
-		files=NULL,
-		scores=scores.tab
-	)
-}
-else if (length(s$files)>1) {
+score.bind=function(s) {if (length(s$files)==0) s else{
+	scores.new=read.csv(s$files[1],header=F,col.names=c('word','instance','correct'))
+	scores.new$id=s$files[1]
 	Recall(list(files=s$files[-1],
-		scores=scores
-	)))
-} else if (is.null(s$files)) { s }
-}
-main=function(){
-	score.bind(list(files=c('scores00.csv','scores00.csv','scores00.csv')))
+		scores=rbind(scores.new,s$scores)))
+}}
+
+#This takes a comma-separated list of file names
+combine=function(...){
+	score.bind(list(files=c(...)))
 }
 
