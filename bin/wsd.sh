9923b77649c7fec880f47b01e773a7a1f3763814
Thomas Levine <thomas.levine@gmail.com>
2011-04-09 20:25:16 -0400

    don't write to a file

--- bin/wsd.sh
+++ bin/wsd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Run this like so
 #score2csv <predictions> <key>
+../classify.py
 
 #Convert to a nice format
-./scorer2 $1 $2  -v|sed -n -e 's/^score for //p'|sed -e 's/: /,/' -e 's/^.*_/"/' -e 's/.bnc./.bnc","/' #|
+../scoring/scorer2 $1 $2  -v|sed -n -e 's/^score for //p'|sed -e 's/: /,/' -e 's/^.*_/"/' -e 's/.bnc./.bnc","/' #|
 #Consider using the predictions file as an identifier for the treatment combination
