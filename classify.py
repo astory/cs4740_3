cbec891bc2b1bfdf9d4e5efbbd616aec26c0029f
Alec Story <avs38@cornell.edu>
2011-04-10 15:25:45 -0400

    decision tree enabled by changing constants

--- classify.py
+++ classify.py
@@ -11,6 +11,12 @@ CUTOFF_PROB = .5
 BOOTSTRAP_CUTOFF_PROB = .8
 BOOTSTRAP_REPS = 5
 
+USE_PROBS = True
+USE_BOOTSTRAP = True # only matters if USE_PROBS is true
+
+#CLASSIFIER=nltk.DecisionTreeClassifier #does not provide a probability measure
+CLASSIFIER=nltk.NaiveBayesClassifier
+
 def assign_features(instance):
 	context = instance['context']
 	pos = instance['position']
@@ -40,11 +46,12 @@ def build_test(instances):
 	return test
 
 def classify(train,test):
-	classifier = nltk.NaiveBayesClassifier.train(train)
+	classifier = CLASSIFIER.train(train)
 	rawSenseList = classifier.batch_classify(test)
-	probDistList = classifier.batch_prob_classify(test)
+	probDistList = classifier.batch_prob_classify(test) if USE_PROBS\
+		else [-1 for x in rawSenseList] #just a placeholder, should not be read
 	return\
-		[dict(sense=sense,prob=prob.prob(sense))\
+		[dict(sense=sense,prob=prob.prob(sense) if USE_PROBS else 1)\
 		for sense,prob in zip(rawSenseList, probDistList)]
 
 def batch_classify(items, tests):
@@ -60,12 +67,13 @@ def batch_classify(items, tests):
 		test=build_test(tests[lexitem])
 
 		# TODO(astory): make dynamic
-		for i in range(BOOTSTRAP_REPS):
-			classified = classify(train,test)
-			for result,test_inst in zip(classified, test):
-				if (result['prob'] > BOOTSTRAP_CUTOFF_PROB and
-					(test_inst, result['sense']) not in train):
-					train.append((test_inst, result['sense']))
+		if USE_PROBS and USE_BOOTSTRAP:
+			for i in range(BOOTSTRAP_REPS):
+				classified = classify(train,test)
+				for result,test_inst in zip(classified, test):
+					if (result['prob'] > BOOTSTRAP_CUTOFF_PROB and
+						(test_inst, result['sense']) not in train):
+						train.append((test_inst, result['sense']))
 
 		senses.extend(classify(train,test))
 	return senses
