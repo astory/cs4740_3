743b8be7be932775a0af81b744819b415bae6b90
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 13:49:35 -0400

    add flags to wsd.sh

--- wsd.sh
+++ wsd.sh
@@ -10,11 +10,11 @@ mkdir experiment/trainingSystem
 
 #Edit the nested for loops to select a series of feature combinations
 run=1
-for classifier in memm bayes tree; do
-  for bootstrap in off on; do #Fix how a lack of bootstrapping for trees doesn't work well here
-    for colocation in off on; do # -l NUM is the colocation distance, 0 is "don't use it"
-      for coocurrence in off on; do # -r enables cooccurrence usae
-        for base_word in off on; do # -e enables base word usage
+for classifier in -m -n -t; do
+  for bootstrap in '' '-b 10' '-b 100' ; do #Fix how a lack of bootstrapping for trees doesn't work well here
+    for colocation in '-l 0' '-l 1' '-l 2'; do # -l NUM is the colocation distance, 0 is "don't use it"
+      for coocurrence in '' -r; do # -r enables cooccurrence usae
+        for base_word in '' -e; do # -e enables base word usage
 #Run the actual system
 ./classify.py $classifier $bootstrap $colocation $coocurrence $unstemmed $dependecyParsing $featureX $featureY > $tmp
 #Convert to a nice format
