69cd73d4f1b69729c4920fe82e5bf33882833660
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 14:54:49 -0400

    save calls in text and run them

---
+++ scripts/run_calls.sh
@@ -0,0 +1,6 @@
+tmp=$1
+run=`echo $tmp|sed s/tmp//`
+
+#Convert to a nice format
+./scoring/scorer2 $tmp $key  -v|sed -n -e 's/^score for //p'|
+sed -e 's/: /,/' -e 's/^.*_/"/' -e 's/.bnc./.bnc","/' > experiment/trainingSystem/trainingSystem_$run.csv
