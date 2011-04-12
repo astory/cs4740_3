a558a2b1ae849c54c5453b75e96ad5725c7b4af5
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 23:35:27 -0400

    fix saving of calls

--- wsd2.sh
+++ wsd2.sh
@@ -2,7 +2,7 @@
 batch=batch$1
 key=EnglishLS.test/EnglishLS.test.key
 mkdir experiment/$batch
-bash scripts/make_calls-smp2.sh $key $batch> experiment/$batch/calls.sh
-cat experiment/$batch/calls.sh|bash scripts/calls2csv.sh > experiment/$batch/calls.csv
-bash experiment/$batch/calls.sh
-bash scripts/score_calls.sh experiment/$batch $key
+bash scripts/make_calls-smp2.sh $key $batch> experiment/$batch/calls2.sh
+cat experiment/$batch/calls2.sh|bash scripts/calls2csv.sh > experiment/$batch/calls2.csv
+bash experiment/$batch/calls2.sh
+bash scripts/score_calls2.sh experiment/$batch $key
--- wsd4.sh
+++ wsd4.sh
@@ -2,7 +2,7 @@
 batch=batch$1
 key=EnglishLS.test/EnglishLS.test.key
 mkdir experiment/$batch
-bash scripts/make_calls-smp4.sh $key $batch> experiment/$batch/calls.sh
-cat experiment/$batch/calls.sh|bash scripts/calls2csv.sh > experiment/$batch/calls.csv
-bash experiment/$batch/calls.sh
-bash scripts/score_calls.sh experiment/$batch $key
+bash scripts/make_calls-smp4.sh $key $batch> experiment/$batch/calls4.sh
+cat experiment/$batch/calls4.sh|bash scripts/calls2csv.sh > experiment/$batch/calls4.csv
+bash experiment/$batch/calls4.sh
+bash scripts/score_calls4.sh experiment/$batch $key
