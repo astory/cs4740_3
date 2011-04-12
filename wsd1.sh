34e4bb89b702933c9dfd03da9f31485ef02782e0
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 23:39:50 -0400

    dunno

--- wsd1.sh
+++ wsd1.sh
@@ -2,7 +2,7 @@
 batch=batch$1
 key=EnglishLS.test/EnglishLS.test.key
 mkdir experiment/$batch
-bash scripts/make_calls-smp1.sh $key $batch> experiment/$batch/calls.sh
-cat experiment/$batch/calls.sh|bash scripts/calls2csv.sh > experiment/$batch/calls.csv
-bash experiment/$batch/calls.sh
+bash scripts/make_calls-smp1.sh $key $batch> experiment/$batch/calls1.sh
+cat experiment/$batch/calls1.sh|bash scripts/calls2csv.sh > experiment/$batch/calls1.csv
+bash experiment/$batch/calls1.sh
 bash scripts/score_calls.sh experiment/$batch $key
