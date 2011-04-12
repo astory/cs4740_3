4347fd0eb7ec6e4ef3cad5ac3cdccd56c04d2e7a
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 20:41:55 -0400

    adjust spelling in script

--- scripts/calls2csv.sh
+++ scripts/calls2csv.sh
@@ -3,7 +3,7 @@
 #./scripts/make_calls.sh |
 
 #Header
-echo 'classifier,bootstrap,colocation,coocurrence,base_word,run_id'
+echo 'classifier,bootstrap,colocation,cooccurrence,base_word,run_id'
 #Fill in empty flags
 sed -e 's+> experiment/trainingSystem/tmp+,+' -e 's/-\([nmt]\)  /-\1 -b 0 /' -e 's/-\([re]\)/-\1 1/g'| sed -e 's/   / -r 0 -e 0/'| sed -e 's/  -e/ -r 0 -e/' -e 's/-r 1  /-r 1 -e 0/'|
 #Format as csv
