b201b7ad20276830d9358315d53a0d0334c76318
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 22:33:05 -0400

    calls2csv takes arbitrary

--- scripts/calls2csv.sh
+++ scripts/calls2csv.sh
@@ -5,6 +5,6 @@
 #Header
 echo 'classifier,bootstrap,colocation,cooccurrence,base_word,run_id'
 #Fill in empty flags
-sed -e 's+> experiment/trainingSystem/tmp+,+' -e 's/-\([nmt]\)  /-\1 -b 0 /' -e 's/-\([re]\)/-\1 1/g'| sed -e 's/   / -r 0 -e 0/'| sed -e 's/  -e/ -r 0 -e/' -e 's/-r 1  /-r 1 -e 0/'|
+sed -e 's+> experiment/[^/]*/tmp+,+' -e 's/-\([nmt]\)  /-\1 -b 0 /' -e 's/-\([re]\)/-\1 1/g'| sed -e 's/   / -r 0 -e 0/'| sed -e 's/  -e/ -r 0 -e/' -e 's/-r 1  /-r 1 -e 0/'|
 #Format as csv
 sed -e 's+^./classify.py -++' -e 's/ -[blre] /,/g' -e 's/ //g'
