d1baa1dae1f3b15d33476aee07f4d0baf4f37864
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 22:24:56 -0400

    wsd.sh runs everything now

--- wsd.sh
+++ wsd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 batch=batch$1
-bash scripts/make_calls.sh > experiment/$batch/calls.sh
+key=EnglishLS.test/EnglishLS.test.key
+mkdir experiment/$batch
+bash scripts/make_calls.sh $key $batch> experiment/$batch/calls.sh
 cat experiment/$batch/calls.sh|bash scripts/calls2csv.sh > experiment/$batch/calls.csv
 bash experiment/$batch/calls.sh
-bash scripts/score_calls.sh experiment/$batch EnglishLS.test/EnglishLS.test.key
+bash scripts/score_calls.sh experiment/$batch $key
