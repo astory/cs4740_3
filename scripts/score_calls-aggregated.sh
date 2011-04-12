404e41cca3ffe2daa94a917d0a529aaa1f418752
Thomas Levine <thomas.levine@gmail.com>
2011-04-12 07:42:20 -0400

    adjust scoring scripts

--- scripts/score_calls-aggregated.sh
+++ scripts/score_calls-aggregated.sh
@@ -7,7 +7,7 @@ tmps=$1
 key=$2
 #run=`echo $tmp|sed s/^.*tmp//`
 echo run_id,precision,recall,attempted
-for tmp in $tmps/tmp[0-9]*; do
+for tmp in $tmps/tmp*[0-9]; do
 	#Convert to a nice format
 	./scoring/scorer2 $tmp EnglishLS.test/EnglishLS.test.key |sed -e 's+^.*score for.*/tmp+"tmp+' -e 's+ using key.*$++' -e 's+(.*)++' -e 's+^.*:++' -e 's/[ %]//g' -e '/^$/d ' |tr '\n' ','|sed 's/,$//'
 	echo ''
