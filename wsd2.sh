32ced60b19e2f4c05ca216d9e6e761f8cbd2fa47
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 22:47:57 -0400

    make_calls-smp

--- wsd2.sh
+++ wsd2.sh
@@ -2,7 +2,7 @@
 batch=batch$1
 key=EnglishLS.test/EnglishLS.test.key
 mkdir experiment/$batch
-bash scripts/make_calls2.sh $key $batch> experiment/$batch/calls.sh
+bash scripts/make_calls-smp2.sh $key $batch> experiment/$batch/calls.sh
 cat experiment/$batch/calls.sh|bash scripts/calls2csv.sh > experiment/$batch/calls.csv
 bash experiment/$batch/calls.sh
 bash scripts/score_calls.sh experiment/$batch $key
