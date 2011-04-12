88a0f4f87dbc2983612189a92c7cb5ce12dd72cb
Thomas Levine <thomas.levine@gmail.com>
2011-04-11 20:13:31 -0400

    simplify scoring

--- scripts/score_calls.sh
+++ scripts/score_calls.sh
@@ -1,7 +1,14 @@
-tmp=$1
-key=$2
-run=`echo $tmp|sed s/^.*tmp//`
+#!/bin/bash
+#This takes
+# 1 the directory with the tmp files and
+# 2 the key file
 
-#Convert to a nice format
-./scoring/scorer2 $tmp $key  -v|sed -n -e 's/^score for //p'|
-sed -e 's/: /,/' -e 's/^.*_/"/' -e 's/.bnc./.bnc","/' > experiment/trainingSystem/trainingSystem_$run.csv
+tmps=$1
+key=$2
+#run=`echo $tmp|sed s/^.*tmp//`
+
+for tmp in $tmps/tmp*[0-9]; do
+	#Convert to a nice format
+	./scoring/scorer2 $tmp $key  -v|sed -n -e 's/^score for //p'|
+	sed -e 's/: /,/' -e 's/^.*_/"/' -e 's/.bnc./.bnc","/' > $tmp.csv
+done
