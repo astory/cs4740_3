835068244f5b500e817c3f86822fac470037d522
Thomas Levine <thomas.levine@gmail.com>
2011-04-12 02:14:07 -0400

    collect aggregate scores
    rather than word-level scores

---
+++ scripts/score_calls-aggregated.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This takes
+# 1 the directory with the tmp files and
+# 2 the key file
+
+tmps=$1
+key=$2
+#run=`echo $tmp|sed s/^.*tmp//`
+echo run_id,precision,recall,attempted
+for tmp in $tmps/tmp[0-9]*; do
+	#Convert to a nice format
+	./scoring/scorer2 experiment/batch5/tmp11 EnglishLS.test/EnglishLS.test.key |sed -e 's+^.*score for.*/tmp+"tmp+' -e 's+ using key.*$++' -e s+(.*)++ -e s+^.*:++ -e 's/[ %]//g'|tr '\n' ',' > $tmp-aggregated.csv
+done
