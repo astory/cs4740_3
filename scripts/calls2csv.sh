9647c907df11af2cb7145e113cc9e913ee63f1fa
Thomas Levine <thomas.levine@gmail.com>
2011-04-12 08:06:30 -0400

    calls2csv

--- scripts/calls2csv.sh
+++ scripts/calls2csv.sh
@@ -3,8 +3,13 @@
 #./scripts/make_calls.sh |
 
 #Header
-echo 'classifier,bootstrap,colocation,cooccurrence,base_word,run_id'
+echo 'classifier,bootstrap,colocation,cooccurrence,base_word,dependency_parsing,run_id'
 #Fill in empty flags
-sed -e '/#!/d' -e 's+> experiment/[^/]*/tmp+,+' -e 's/-\([nmt]\)  /-\1 -b 0 /' -e 's/-\([re]\)/-\1 1/g'| sed -e 's/   / -r 0 -e 0/'| sed -e 's/  -e/ -r 0 -e/' -e 's/-r 1  /-r 1 -e 0/'|
+sed -e '/#!/d' -e 's+> experiment/[^/]*/tmp+,+'|
+sed -e 's/  / -z 0 /g'|
+sed -e 's/  / -z 0 /g'|
+sed -e 's/\(-[ablrepz]\) -/\1 1 -/g'|
+sed -e 's/\(-[ablrepz]\) -/\1 1 -/g'|
+
 #Format as csv
-sed -e 's+^./classify.py -++' -e 's/ -[ablrep]* /,/g' -e 's/ //g'
+sed -e 's+^./classify.py -++' -e 's/-[ablrep]*/,/g' -e 's/ //g'
